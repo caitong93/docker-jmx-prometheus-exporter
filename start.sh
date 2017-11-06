5e97a7008488bfd6c5d7dbae1cbe98a8d6667c0f
caitong93 <caitong93@gmail.com>
2017-11-06 19:55:46 +0800

    feat: add JMX_HOST, JMX_PORTenv

--- start.sh
+++ start.sh
@@ -8,5 +8,7 @@ if [ -z "$JVM_OPTS" ]; then
   JVM_OPTS="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555"
 fi
 
+JMX_HOST=${JMX_HOST:-localhost} JMX_PORT=${JMX_PORT:-5555} gomplate -f /opt/jmx_exporter/config.yml.template > /opt/jmx_exporter/config.yml
+
 java $JVM_OPTS -jar /opt/jmx_exporter/jmx_prometheus_httpserver-$VERSION-jar-with-dependencies.jar $SERVICE_PORT /opt/jmx_exporter/config.yml
 
